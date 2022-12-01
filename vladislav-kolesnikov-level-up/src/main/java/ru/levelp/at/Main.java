4c560097263e20467e555f895ef4ad569ae3c275
Vladislav Kolesnikov <kolxy@ya.ru>
2022-12-01 22:17:39 +0300

    edited

--- vladislav-kolesnikov-level-up/src/main/java/ru/levelp/at/Main.java
+++ vladislav-kolesnikov-level-up/src/main/java/ru/levelp/at/Main.java
@@ -1,8 +1,14 @@
 package ru.levelp.at;
 
+import static ru.levelp.at.LuckyTicket.isLuckyTicket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //test pull
+        String ticket = "321123";
+        if (isLuckyTicket(ticket)) {
+            System.out.println("It's a lucky ticket!");
+        } else {
+            System.out.println("This is not a lucky ticket");
+        }
     }
 }
