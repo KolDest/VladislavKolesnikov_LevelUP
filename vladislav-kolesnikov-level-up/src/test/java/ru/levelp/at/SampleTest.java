76222c77a6f32ad1bc64abb932af01f3b5403900
Vladislav Kolesnikov <kolxy@ya.ru>
2022-12-05 00:08:43 +0300

    Updated 1

---
+++ vladislav-kolesnikov-level-up/src/test/java/ru/levelp/at/SampleTest.java
@@ -0,0 +1,11 @@
+package ru.levelp.at;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+    @Test
+    void sample() {
+        Assertions.assertEquals(4, 2 + 2);
+    }
+}
